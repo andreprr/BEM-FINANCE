af31b25387d6137a69133e16021272596d88febc
andreprr <efsa.andre@gmail.com>
2024-02-04 21:31:05 +0700

    bemfinance

---
+++ bemfinance.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 04, 2024 at 03:16 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bemfinance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank`
+--
+
+CREATE TABLE `bank` (
+  `bank_id` int(11) NOT NULL,
+  `bank_nama` varchar(255) NOT NULL,
+  `bank_nomor` varchar(255) NOT NULL,
+  `bank_pemilik` varchar(255) NOT NULL,
+  `bank_saldo` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hutang`
+--
+
+CREATE TABLE `hutang` (
+  `hutang_id` int(11) NOT NULL,
+  `hutang_tanggal` date NOT NULL,
+  `hutang_nominal` int(11) NOT NULL,
+  `hutang_keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `kategori_id` int(11) NOT NULL,
+  `kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`kategori_id`, `kategori`) VALUES
+(1, '01. LAINNYA'),
+(2, '02. DEPT. KOMINFO'),
+(3, '03. DEPT. PENDIDIKAN'),
+(4, '04. DEPT. EKONOMI'),
+(5, '05. DEPT. SOSIAL'),
+(6, '06. DEPT. AGAMA'),
+(7, '07. DEPT. OLAHRAGA'),
+(10, '08. WARBEM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `piutang`
+--
+
+CREATE TABLE `piutang` (
+  `piutang_id` int(11) NOT NULL,
+  `piutang_tanggal` date NOT NULL,
+  `piutang_nominal` int(11) NOT NULL,
+  `piutang_keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `transaksi_id` int(11) NOT NULL,
+  `transaksi_tanggal` date NOT NULL,
+  `transaksi_jenis` enum('Pengeluaran','Pemasukan') NOT NULL,
+  `transaksi_kategori` int(11) NOT NULL,
+  `transaksi_nominal` int(11) NOT NULL,
+  `transaksi_keterangan` text NOT NULL,
+  `transaksi_foto` varchar(255) NOT NULL,
+  `transaksi_bank` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_nama` varchar(100) NOT NULL,
+  `user_username` varchar(100) NOT NULL,
+  `user_password` varchar(100) NOT NULL,
+  `user_foto` varchar(100) DEFAULT NULL,
+  `user_level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_nama`, `user_username`, `user_password`, `user_foto`, `user_level`) VALUES
+(1, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'photo.jpg', 'administrator'),
+(3, 'andre', 'razerfear', 'dd573120e473c889140e34e817895495', '2112588994_STIMIK.png', 'manajemen');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bank`
+--
+ALTER TABLE `bank`
+  ADD PRIMARY KEY (`bank_id`);
+
+--
+-- Indexes for table `hutang`
+--
+ALTER TABLE `hutang`
+  ADD PRIMARY KEY (`hutang_id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`kategori_id`);
+
+--
+-- Indexes for table `piutang`
+--
+ALTER TABLE `piutang`
+  ADD PRIMARY KEY (`piutang_id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`transaksi_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bank`
+--
+ALTER TABLE `bank`
+  MODIFY `bank_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `hutang`
+--
+ALTER TABLE `hutang`
+  MODIFY `hutang_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `kategori_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `piutang`
+--
+ALTER TABLE `piutang`
+  MODIFY `piutang_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `transaksi_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
